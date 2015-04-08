86d05b589a7925df9f3d84689c1906f435ac2819
pg205 <vicenteortegaaguado@gmail.com>
2015-04-08 20:00:57 +0200

    clase2_tarea0

---
+++ tarea0/tarea0/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  tarea0
+//
+//  Created by villeguillo on 8/4/15.
+//  Copyright (c) 2015 villeguillo. All rights reserved.
+//
+
+import Foundation
+
+var valor1 = 1, valor2 = 2, valor3 = 3
+
+var entero:Int
+
+var primero, segundo, tercero:Int
