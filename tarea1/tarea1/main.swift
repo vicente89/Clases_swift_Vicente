3165b95f9c91d0cae09f49a9fef827e6c32ff3af
pg205 <vicenteortegaaguado@gmail.com>
2015-04-09 10:07:11 +0200

    clase2_tarea1
    corregida

---
+++ tarea1/tarea1/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  tarea1
+//
+//  Created by villeguillo on 8/4/15.
+//  Copyright (c) 2015 villeguillo. All rights reserved.
+//
+
+import Foundation
+
+let valor = -45.11
+
+// swift establece que la variable valor sea Double, por que es una constante decimal y hay dos tipos de variables decimales Float de 32 bits y Double de 64 bits, swift se quita posibles problemas y asigna la variable mas grande para asegurarse una correcta compilacion y ejecucion en este caso Double.
+
+
+
+
+
