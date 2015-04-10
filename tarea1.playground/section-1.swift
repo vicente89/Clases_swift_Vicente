9e1cf515fb9fa799bd0cfc321ffd31e2d7dcb0be
pg205 <vicenteortegaaguado@gmail.com>
2015-04-10 20:03:02 +0200

    tarea1

---
+++ tarea1.playground/section-1.swift
@@ -0,0 +1,7 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let valor = -45.11
+
+// swift establece que la variable valor sea Double, por que es una constante decimal y hay dos tipos de variables decimales Float de 32 bits y Double de 64 bits, swift se quita posibles problemas y asigna la variable mas grande para asegurarse una correcta compilacion y ejecucion en este caso Double.
